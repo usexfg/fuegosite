2b2b8d65c4d48f116e1ee11552a2a466a69464ba
ᗧ···ᗣ···ᗣ <54386876+ColinRitman@users.noreply.github.com>
2025-01-19 02:28:17 -0500

    Update mktcaphero.js
    basically revert last changes

--- assets/js/mktcaphero.js
+++ assets/js/mktcaphero.js
@@ -21,36 +21,32 @@
 
     let intervalId;
 
-    async function updateValues() {
-      const fuegoUrl = "https://graphsv2.coinpaprika.com/currency/data/xfg-fango/7d/?quote=usd";
-      const fuegoData = await fetchData(fuegoUrl);
-
-      if (fuegoData) {
-        try {
-          const closingPrice = fuegoData.data[0].price_high; // Access closing price using updated API structure
-          const fixedRate = 8000008;
-
-          if (typeof closingPrice === 'number') {
-            const product = closingPrice * fixedRate;
-            displayResult(closingPrice, product);
-          } else {
-            console.error("API response did not contain a valid 'price_high' property.");
-            console.log("Fuego Data:", fuegoData);
-          }
-        } catch (error) {
-          console.error("Error processing API data:", error);
-        }
-      } else {
-        console.error("No data received or empty array from API");
-        console.log("Fuego Data:", fuegoData);
-      }
-    }
-
     function startUpdates() {
-      // Call updateValues() immediately on page load
-      updateValues();
+      intervalId = setInterval(async () => {
+        // CoinPaprika API for Fuego (XFG) closing price
+        const fuegoUrl = "https://graphsv2.coinpaprika.com/currency/data/xfg-fango/7d/?quote=usd";
+        const fuegoData = await fetchData(fuegoUrl);
 
-      intervalId = setInterval(updateValues, 30000); // Set the interval for subsequent updates
+        if (fuegoData) {
+          try {
+            // Extract the closing price
+            const closingPrice = fuegoData[0].price_high;
+
+            // xfg total supply
+            const fixedRate = 8000008;
+
+            if (typeof closingPrice === 'number') {
+              const product = closingPrice * fixedRate;
+              displayResult(product);
+            } else {
+              console.error("API response did not contain 'close' property.");
+              console.log("Fuego Data:", fuegoData);
+            }
+          } catch (error) {
+            console.error("Error processing API data:", error);
+          }
+        }
+      }, 30000); // Update every 30 seconds 
     }
 
     startUpdates(); // Start updates when the page loads
