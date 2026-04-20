0f041196ef82283f590b92e3de8e294733a6f9b3
ColinRitman <aejontargaryen@pm.me>
2026-04-19 20:47:12 -0400

    math

--- src/Common/Math.cpp
+++ src/Common/Math.cpp
@@ -15,7 +15,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Fuego. If not, see <https://www.gnu.org/licenses/>.
 
-#include "Math.h"
+#include "MathUtils.h"
 
 namespace {
 char suppressMSVCWarningLNK4221;
